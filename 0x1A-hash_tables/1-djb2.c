79fd65f2b515f6ca25cf7eca7d19d0e1abb07aaf
David Onyemaenu <david.onyemaenu@gmail.com>
2023-04-07 20:23:01 -0700

    1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -5,12 +5,12 @@
  * Return: hash key
  */
 unsigned long int hash_djb2(const unsigned char *str)
-{
-    unsigned long hash = 5381;
-    int q;
+{ 
+	unsigned long hash = 5381;
+	int q;
 
-    while ((q = *str++))
-        hash = ((hash << 5) + hash) + q; /* hash * 33 + q */
+        while ((q = *str++))
+		hash = ((hash << 5) + hash) + q; /* hash * 33 + q */
 
-    return (hash);
+    	return (hash);
 }
